98aea1969888271cc4291e3b368f15169e3631c0
Ibnu Mahjub <ibnumahjub23@gmail.com>
2024-03-27 09:40:38 +0700

    first commit

---
+++ hello_rust/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+    println!("How");
+    println!("Are");
+    println!("You");
+}
---
+++ variabel/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // let coba_variabel = "oke gas";
+
+    // println!("{}", coba_variabel);
+
+    let pesan_nomer = 1;
+    let pesan_dua = "Coba-rust-lang";
+    print!("pesan nomer {}: {}", pesan_nomer,pesan_dua);
+}
